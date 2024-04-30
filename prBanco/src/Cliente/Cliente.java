084a2da742c48df8288ea45c755a5497610cc311
ddc0010 <ddc0010@alu.medac.es>
2024-04-30 17:43:12 +0200

    Constructor Cliente creado

--- prBanco/src/Cliente/Cliente.java
+++ prBanco/src/Cliente/Cliente.java
@@ -2,4 +2,16 @@ package Cliente;
 
 public class Cliente {
 
+	String nombre; 
+	String apellidos; 
+	int edad;
+	
+	public Cliente(String nombre, String apellidos, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+	} 
+	
+
 }
